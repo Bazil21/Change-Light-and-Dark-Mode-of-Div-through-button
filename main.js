3e05f736ed97d003dbadaf18fa32114b70232e0d
Bazil21 <59576110+Bazil21@users.noreply.github.com>
2020-09-12 20:08:54 +0500

    Light and Dark Mode

---
+++ main.js
@@ -0,0 +1,16 @@
+var lightbtn = document.getElementById("lightbtn");
+var darkbtn = document.getElementById("darkbtn");
+var lorem = document.getElementById("lorem");
+
+
+function lightMode(){
+    
+    lorem.style.backgroundColor = "white";
+    lorem.style.color = "black"
+}
+
+
+function darkMode(){
+    lorem.style.backgroundColor = "#485460";
+    lorem.style.color = "white"
+}
